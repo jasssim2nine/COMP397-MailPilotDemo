dd1cd44b5b894980192375331083dde08c2d3c12
Jasim Khan <jasim.khan89@gmail.com>
2018-07-15 16:35:37 -0400

    recoded play scene

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -12,6 +12,7 @@ var scenes;
 (function (scenes) {
     var PlayScene = /** @class */ (function (_super) {
         __extends(PlayScene, _super);
+        // Private Instance Variables
         // Public Properties
         //Constructor
         function PlayScene(assetManager) {
@@ -20,29 +21,14 @@ var scenes;
             return _this;
         }
         //Private Methods
-        PlayScene.prototype._nextButtonClick = function () {
-            objects.Game.currentScene = config.Scene.OVER;
-        };
-        PlayScene.prototype._backButtonClick = function () {
-            objects.Game.currentScene = config.Scene.START;
-        };
         //Public Methods
         //Initialize game variables and objects
         PlayScene.prototype.Start = function () {
-            this._playLabel = new objects.Label("Game Play", "45px", "consolas", "#000000", 320, 240, true);
-            this._nextButton = new objects.Button(this.assetManager, "nextButton", 450, 400);
-            this._backButton = new objects.Button(this.assetManager, "backButton", 150, 400);
             this.Main();
         };
         PlayScene.prototype.Update = function () {
         };
         PlayScene.prototype.Main = function () {
-            this.addChild(this._playLabel); //add play label to the scene
-            this.addChild(this._nextButton); //add next button
-            this.addChild(this._backButton); //add back button
-            //click events for next button and back button 
-            this._backButton.on("click", this._backButtonClick);
-            this._nextButton.on("click", this._nextButtonClick);
         };
         return PlayScene;
     }(objects.Scene));
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -1,9 +1,7 @@
 module scenes {
     export class PlayScene extends objects.Scene {
         // Private Instance Variables
-        private _playLabel: objects.Label;
-        private _nextButton: objects.Button;
-        private _backButton: objects.Button;
+        
 
         // Public Properties
 
@@ -16,24 +14,13 @@ module scenes {
 
 
         //Private Methods
-        private _nextButtonClick(): void {
-
-            objects.Game.currentScene = config.Scene.OVER;
-        }
-
-        private _backButtonClick(): void {
-
-            objects.Game.currentScene = config.Scene.START;
-        }
+        
 
         //Public Methods
 
         //Initialize game variables and objects
         public Start(): void {
-            this._playLabel = new objects.Label("Game Play", "45px", "consolas", "#000000", 320, 240, true);
-            this._nextButton = new objects.Button(this.assetManager, "nextButton", 450, 400);
-            this._backButton = new objects.Button(this.assetManager, "backButton", 150, 400);
-
+           
             this.Main();
 
         }
@@ -44,13 +31,7 @@ module scenes {
 
         }
         public Main(): void {
-            this.addChild(this._playLabel);//add play label to the scene
-            this.addChild(this._nextButton);//add next button
-            this.addChild(this._backButton);//add back button
-            
-            //click events for next button and back button 
-            this._backButton.on("click",this._backButtonClick);
-            this._nextButton.on("click", this._nextButtonClick);
+          
         }
 
 
